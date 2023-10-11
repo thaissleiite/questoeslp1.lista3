1c20611e421f08779773bbfe76b2ca1b475d461c
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-10-11 11:12:48 -0300

    Update q2.c

--- q2.c
+++ q2.c
@@ -41,3 +41,49 @@ return 0;
 
     }
 
+
+
+//codigo modificado + com erro - não aparece a parte de limite exedido
+int main() {
+ int nconta;
+    float sinicial, tcobrancas, tcreditos, lcredito;
+
+    while(1){
+    printf("Insira o valor da conta(0 para finalizar): ");
+    scanf("%d", &nconta);
+
+       if(nconta == 0){
+
+        break;
+
+       }
+
+       printf("Insira o saldo inicial: ");
+       scanf("%f", &sinicial);
+       printf("Insira o total de cobrancas: ");
+       scanf("%f", &tcobrancas);
+       printf("Insira o total de creditos: ");
+       scanf("%f", &tcreditos);
+       printf("Insira o limite de credito: ");
+       scanf("%f", &lcredito);
+
+       float nsaldo;
+       nsaldo = sinicial + tcobrancas - tcreditos;
+
+        
+             printf("O valor da conta eh: %d\n", nconta);
+             printf("O limite de credito eh: %.2f\n", lcredito);
+             printf("O novo saldo eh: %.2f\n", nsaldo);
+
+         if(sinicial > lcredito){
+             printf("Limite de creditos exedidos\n");
+
+       }
+        
+
+       }
+
+return 0;
+
+    }
+
